012d9a5a901881df6d04d4e9d6054407bbf9de31
Suguru Inatomi <suguru.inatomi@gmail.com>
2018-08-23 11:37:11 +0900

    feat: update content

--- src/app/view/page/home/home.module.ts
+++ src/app/view/page/home/home.module.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCardModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatDividerModule, MatIconModule, MatListModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { GridLayoutModule } from '@lacolaco/ngx-grid-layout';
 import { HomePageComponent } from './home.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, GridLayoutModule, MatButtonModule, MatCardModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    GridLayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+  ],
   declarations: [HomePageComponent],
 })
 export class HomeModule {}
