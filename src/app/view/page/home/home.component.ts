c65f744ed3d810370b7f5a481157c02235f2de44
Suguru Inatomi <suguru.inatomi@gmail.com>
2018-08-23 10:20:39 +0900

    refactor: renewal home page

--- src/app/view/page/home/home.component.ts
+++ src/app/view/page/home/home.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePageComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class HomePageComponent {}
