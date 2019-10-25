8d990a89192f8b5f8561f3fa22e3e938aa1a41d7
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-10-25 10:22:51 +0900

    Angular CLI update @angular/material@next

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -21,9 +21,6 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
-/** IE10 and IE11 requires the following for the Reflect API. */
-import 'core-js/es6/reflect';
-
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 
