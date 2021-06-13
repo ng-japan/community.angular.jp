c57863ca5bee2f82a28a03a31b4f3c941872601b
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-06-13 13:45:53 +0900

    fix

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   // `import.meta` requires ESM support
   preset: 'jest-preset-angular/presets/defaults-esm',
-  moduleNameMapper: {
-    tslib: 'tslib/tslib.es6.js',
-  },
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
 };
