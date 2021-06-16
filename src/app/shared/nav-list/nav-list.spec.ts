d13b2072d5a089d302cb1d933a0d6531f0dcd6ec
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-06-16 17:11:02 +0900

    test: solve todo spec

--- src/app/shared/nav-list/nav-list.spec.ts
+++ src/app/shared/nav-list/nav-list.spec.ts
@@ -1,3 +1,41 @@
+import { By } from '@angular/platform-browser';
+import { RouterLinkActive } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { render } from '@testing-library/angular';
+import { NavListDirective, NavListItemDirective } from './nav-list';
+
 describe('NavListDirective', () => {
-  it.todo('should render');
+  it('should render', async () => {
+    const { getByRole } = await render(`<div appNavList ></div>`, {
+      declarations: [NavListDirective],
+    });
+    expect(getByRole('list')).toHaveClass('flex flex-col py-2');
+  });
+});
+
+describe('NavListItemDirective', () => {
+  it('should render', async () => {
+    const { getByRole } = await render(`<div appNavListItem ></div>`, {
+      declarations: [NavListItemDirective],
+    });
+    expect(getByRole('listitem')).toHaveClass(
+      'flex items-center gap-2 h-12 px-4 no-underline text-black hover:bg-gray-100',
+    );
+  });
+
+  it('should change background color if active', async () => {
+    const {
+      getByRole,
+      detectChanges,
+      fixture: { debugElement },
+    } = await render(`<div appNavListItem routerLink routerLinkActive></div>`, {
+      declarations: [NavListItemDirective],
+      imports: [RouterTestingModule],
+    });
+    const rla = debugElement.query(By.directive(RouterLinkActive)).injector.get(RouterLinkActive);
+    (rla as any).isActive = true;
+    detectChanges();
+
+    expect(getByRole('listitem')).toHaveClass('bg-gray-100');
+  });
 });
--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -1,3 +1,4 @@
 import 'jest-preset-angular/setup-jest';
+import '@testing-library/jest-dom';
 
 Object.defineProperty(window.navigator, 'userAgent', { value: 'test' });
