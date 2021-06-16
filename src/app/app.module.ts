5acc222d0057c5ce8ff9ef122d96789ff1742a4e
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-06-16 17:49:42 +0900

    refactor: replace mat-toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RendererModule, TransferHttpCacheModule } from '@nguniversal/common/clover';
@@ -22,7 +19,6 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     TransferHttpCacheModule,
 
-    MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
     SharedModule,
