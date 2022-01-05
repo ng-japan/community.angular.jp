42107ccbabad7e4de4266d79e073aa7222cbcceb
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-01-05 13:33:16 +0900

    fix: disable worker usage

--- src/app/shared/markdown/index.ts
+++ src/app/shared/markdown/index.ts
@@ -1,4 +1,4 @@
-import { wrap } from 'comlink';
+// import { wrap } from 'comlink';
 
 type MarkdownWorker = typeof import('./markdown.worker').api;
 
@@ -6,11 +6,12 @@ let worker: MarkdownWorker;
 
 export async function processMarkdown(source: string): Promise<string> {
   if (worker == null) {
-    if (window.Worker) {
-      worker = wrap<MarkdownWorker>(new Worker(new URL('./markdown.worker', import.meta.url)));
-    } else {
-      worker = await import('./markdown.worker').then((m) => m.api);
-    }
+    // Note: Angular v13 + WebWorker + Jest doesn't work due to ESM support of Node.js
+    // if (window.Worker) {
+    //   worker = wrap<MarkdownWorker>(new Worker(new URL('./markdown.worker', import.meta.url)));
+    // } else {
+    // }
+    worker = await import('./markdown.worker').then((m) => m.api);
   }
   return await worker.processMarkdown(source);
 }
