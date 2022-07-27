3c96320a5906cdac0cd385acd0b0e570793e9e1d
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-07-27 11:58:20 +0900

    build(deps): ng update v14.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   prefix: '',
   mode: 'jit',
-  purge: {
-    content: ['./src/**/*.{html,ts,css,scss,sass,less,styl}'],
-  },
+  content: ['./src/**/*.{html,ts,css,scss,sass,less,styl}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
